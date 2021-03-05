3db34104847c6f4d523a6017a09a6c8f8fe6aa2c
radmir-shkred <psitecton@gmail.com>
2021-03-05 17:04:58 +0200

    sixth commit

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './app.css';
+import {withBookstoreService} from '../hoc';
 
-function App(props) {
+function App({bookstoreService}) {
+    console.log(bookstoreService.getBooks());
     return (
         <div>App</div>
     );
 }
 
-export default App;
+export default withBookstoreService()(App);

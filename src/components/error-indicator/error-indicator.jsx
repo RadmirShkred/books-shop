a2ee8eaadeb9aa27be598b18090ccb0627098fb2
radmir-shkred <psitecton@gmail.com>
2021-03-05 14:06:21 +0200

    fourth commit

---
+++ src/components/error-indicator/error-indicator.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './error-indicator.css';
+
+function ErrorIndicator(props) {
+    return (
+        <div>Error!</div>
+    );
+}
+
+export default ErrorIndicator;

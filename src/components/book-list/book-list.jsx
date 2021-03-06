6dea7899118579d2a73b02342167ba1e37679369
RadmirShkred <psitecton@gmail.com>
2021-03-06 21:02:50 +0300

    ninth commit

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -2,8 +2,19 @@ import React, {Component} from 'react';
 import './book-list.css';
 import BookListItem from "../book-list-item";
 import {connect} from "react-redux";
+import {withBookstoreService} from "../hoc";
 
 class BookList extends Component {
+
+    componentDidMount() {
+        // 1. receive data
+        const {bookstoreService} = this.props;
+        const data = bookstoreService.getBooks();
+        console.log(data);
+        // 2. dispatch action to store
+
+    }
+
     render() {
         const {books} = this.props;
         return (
@@ -18,4 +29,4 @@ const mapStateToProps = ({books}) => {
     return {books}
 };
 
-export default connect(mapStateToProps)(BookList);
+export default withBookstoreService()(connect(mapStateToProps)(BookList));

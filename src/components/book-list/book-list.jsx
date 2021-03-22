2dcad13197772b217296fa9021fb50bf806a0533
RadmirShkred <psitecton@gmail.com>
2021-03-22 16:48:11 +0300

    13-th commit

--- src/components/book-list/book-list.jsx
+++ src/components/book-list/book-list.jsx
@@ -7,6 +7,7 @@ import {bookAddedToCart, fetchBooks} from "../../actions";
 import {compose} from "../../utils";
 import Spinner from "../spinner";
 import ErrorIndicator from "../error-indicator";
+import {bindActionCreators} from "redux";
 
 const BookList = ({books, onAddedToCart}) => {
     return (
@@ -44,14 +45,14 @@ class BookListContainer extends Component {
     }
 }
 
-const mapStateToProps = ({books, loading, error}) => {
+const mapStateToProps = ({bookList: {books, loading, error}}) => {
     return {books, loading, error};
 };
 const mapDispatchToProps = (dispatch, {bookstoreService}) => {
-    return {
-        fetchBooks: fetchBooks(bookstoreService, dispatch),
-        onAddedToCart: (id) => dispatch(bookAddedToCart(id))
-    }
+    return bindActionCreators({
+        fetchBooks: fetchBooks(bookstoreService),
+        onAddedToCart: bookAddedToCart
+    }, dispatch);
 };
 
 export default compose(
--- src/store.js
+++ src/store.js
@@ -1,7 +1,21 @@
-import {createStore} from 'redux';
-
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(reducer);
+const logMiddleware = () => (next) => (action) => {
+    console.log(action.type);
+    return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
+    }
+    return next(action);
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, stringMiddleware, logMiddleware));
 
 export default store;
